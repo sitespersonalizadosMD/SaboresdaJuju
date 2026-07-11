ef72aa2dd92f4aa7d3b0c1b23fb2eff832667ce3
sitespersonalizadosMD <matheusrodrigues3001@gmail.com>
2026-07-11 11:45:01 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -383,3 +383,49 @@ INICIALIZAÇÃO
 =========================================*/
 
 atualizarCarrinho();
+
+const produtos = [
+
+    {
+        id:1,
+        nome:"Bife à Cavalo",
+        preco:22,
+        imagem:"img/bife.jpg"
+    },
+
+    {
+        id:2,
+        nome:"Frango à Parmegiana",
+        preco:18,
+        imagem:"img/parmegiana.jpg"
+    },
+
+    {
+        id:3,
+        nome:"Frango Acebolado",
+        preco:18,
+        imagem:"img/acebolado.jpg"
+    },
+
+    {
+        id:4,
+        nome:"Frango à Milanesa",
+        preco:18,
+        imagem:"img/milanesa.jpg"
+    },
+
+    {
+        id:5,
+        nome:"Omelete",
+        preco:16,
+        imagem:"img/omelete.jpg"
+    },
+
+    {
+        id:6,
+        nome:"Calabresa Acebolada",
+        preco:16,
+        imagem:"img/calabresa.jpg"
+    }
+
+];
