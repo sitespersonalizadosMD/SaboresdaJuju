005b510b4a29b20907bb3b1095812e138f1773bb
sitespersonalizadosMD <matheusrodrigues3001@gmail.com>
2026-07-11 14:52:52 -0300

    Create produtos.js

---
+++ js/produtos.js
@@ -0,0 +1,63 @@
+const produtos = [
+
+    {
+        id: 1,
+        nome: "Bife à Cavalo",
+        preco: 22
+    },
+
+    {
+        id: 2,
+        nome: "Frango à Parmegiana",
+        preco: 18
+    },
+
+    {
+        id: 3,
+        nome: "Frango Acebolado",
+        preco: 18
+    },
+
+    {
+        id: 4,
+        nome: "Frango à Milanesa",
+        preco: 18
+    },
+
+    {
+        id: 5,
+        nome: "Omelete",
+        preco: 16
+    },
+
+    {
+        id: 6,
+        nome: "Calabresa Acebolada",
+        preco: 16
+    }
+
+];
+
+const acompanhamentos = [
+
+    "Arroz Branco",
+
+    "Arroz Integral",
+
+    "Feijão Preto",
+
+    "Feijão Mulatinho",
+
+    "Macarrão"
+
+];
+
+const finalizacoes = [
+
+    "Batata Frita",
+
+    "Salada Verde",
+
+    "Purê de Batata"
+
+];
